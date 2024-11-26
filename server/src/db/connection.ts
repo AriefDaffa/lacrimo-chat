34c6cd2614a1ad0c4b402df00eb83f3eff57724c
AriefDaffa <ariefkeren76@yahoo.com>
2024-11-26 16:23:56 +0700

    fix: migrate function

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Pool } from 'pg';
 
 import { credentials } from '../../drizzle.config';
@@ -7,3 +8,18 @@ import { logger } from '../common/logger';
 const pool = new Pool(credentials);
 
 export const db = drizzle(pool, { logger });
+
+async function main() {
+  console.log('Starting migration...');
+  await migrate(db, {
+    migrationsFolder: 'drizzle',
+  });
+
+  console.log('Migration complete.');
+  process.exit(0);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(0);
+});

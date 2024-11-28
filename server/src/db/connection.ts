c4be7d9cca58c748b133339d2e8733e01deead5c
AriefDaffa <ariefkeren76@yahoo.com>
2024-11-28 08:19:25 +0700

    fix: error dockerfile

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
+// import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Pool } from 'pg';
 
 import { credentials } from '../../drizzle.config';
@@ -9,17 +9,17 @@ const pool = new Pool(credentials);
 
 export const db = drizzle(pool, { logger });
 
-async function main() {
-  console.log('Starting migration...');
-  await migrate(db, {
-    migrationsFolder: 'drizzle',
-  });
+// async function main() {
+//   console.log('Starting migration...');
+//   await migrate(db, {
+//     migrationsFolder: 'drizzle',
+//   });
 
-  console.log('Migration complete.');
-  process.exit(0);
-}
+//   console.log('Migration complete.');
+//   process.exit(0);
+// }
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(0);
-});
+// main().catch((err) => {
+//   console.error(err);
+//   process.exit(0);
+// });
